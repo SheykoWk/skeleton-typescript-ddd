43a966a6f7d502a886d89652d8ca6440ff040641
SheykoWk <sheyko.wk@gmail.com>
2025-03-31 00:13:30 -0600

    refactor(core): change the query handlers from repository to services

--- src/users/application/handler/list-user-by-id-query.handler.ts
+++ src/users/application/handler/list-user-by-id-query.handler.ts
@@ -3,17 +3,15 @@ import { ListUserByIdQuery } from "../queries/list-user-by-id.query";
 import { UserRepository } from "users/domain/repositories/user.repository";
 import { User } from "users/domain/entities/user.entity";
 import { UserId } from "users/domain/value-objects/user-id.value-object";
+import { UserService } from "users/domain/services/user.service";
 
 @QueryHandler(ListUserByIdQuery)
 export class ListUserByIdQueryHandler implements IQueryHandler<ListUserByIdQuery> {
-	constructor(
-		private readonly userRepository: UserRepository
-	){
+	constructor(private readonly userService: UserService){}
 
-	}
 
 	execute(query: ListUserByIdQuery): Promise<User> {
 		const userId = new UserId(query.id)
-		return this.userRepository.findById(userId)
+		return this.userService.getUserById(userId)
 	}
 }
--- src/users/application/handler/list-users-query.handler.ts
+++ src/users/application/handler/list-users-query.handler.ts
@@ -2,11 +2,12 @@ import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { ListUsersQuery } from "../queries/list-users.query";
 import { UserRepository } from "users/domain/repositories/user.repository";
 import { User } from "users/domain/entities/user.entity";
+import { UserService } from "users/domain/services/user.service";
 
 @QueryHandler(ListUsersQuery)
 export class ListUsersQueryHandler implements IQueryHandler<ListUsersQuery>{
-	constructor(private readonly userRepository: UserRepository){}
+	constructor(private readonly userService: UserService){}
 	execute(query: ListUsersQuery): Promise<User[]> {
-		return this.userRepository.findAll()
+		return this.userService.getAllUsers()
 	}
 }
